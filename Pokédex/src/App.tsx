300298aec32bb16613e9bbfce67c30be3056efbb
William-Dev <williamlegall5@gmail.com>
2024-12-11 15:52:31 +0100

    create props.1

--- Pokédex/src/App.tsx
+++ Pokédex/src/App.tsx
@@ -1,12 +1,22 @@
-import "./App.css";
-import PokemonCard from "./components/PokemonCard"; // Correct
+import React from "react";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc: "",
+    },
+  ];
+
   return (
     <div>
-      <div className="div">
-        <PokemonCard />
-      </div>
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
